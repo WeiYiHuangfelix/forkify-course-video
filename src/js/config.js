dfa7ee6ed025953faf247977b5a9c87b32f12251
WeiYiHuangfelix <a0908882001@gmail.com>
2024-12-03 10:56:35 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "ac72512a-4835-45af-8426-9b7383049c09";
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,17 @@
+import View from './View.js';
+import previewView from './previewView.js'; // 確保正確導入 previewView 模組
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again.';
+  _message = '';
+
+  _generateMarkup() {
+    return this._data
+      .map(result => previewView.render(result, false))
+      .join('');
+  }
+}
+
+export default new ResultsView();
